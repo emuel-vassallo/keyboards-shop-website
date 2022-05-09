2c25bf7d3b6a7f1a2bae391debfae4857ff8fce9
emuel-vassallo <emuelvassallo15@gmail.com>
2022-05-09 23:42:18 +0200

    add styling to center footer items and credit to kbdfanss

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-$('.carousel').flickity({
-  autoPlay: true,
-  contain: true,
-  wrapAround: true,
-  lazyLoad: true,
-});
-
 function addToCart() {
-  alert("Product was added to cart.")
+  alert('Product was added to cart.');
 }
+
+$(document).ready(function () {
+  $('#submit').click(function () {
+    let fullName = $('#fullName').val();
+    let emailAddress = $('#emailAddress').val();
+    let contactNumber = $('#contactNumber').val();
+    let comment = $('#comment').val();
+    $('#returnmessage').empty();
+    if (
+      fullName == '' ||
+      emailAddress == '' ||
+      contactNumber == '' ||
+      comment == ''
+    ) {
+      alert('Please Fill Required Fields');
+    } else {
+      location.href = 'mailto:emuel.vassallo.g52404@mcast.edu.mt';
+    }
+  });
+  $('.carousel').flickity({
+    autoPlay: true,
+    contain: true,
+    wrapAround: true,
+    lazyLoad: true,
+  });
+});
