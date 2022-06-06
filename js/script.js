7417150693cb460bd12a4943b1e3b2cf04a43a03
emuel-vassallo <emuelvassallo15@gmail.com>
2022-06-06 22:02:13 +0200

    fix regex.text typo

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ $(document).ready(() => {
   // https://melvingeorge.me/blog/check-if-string-is-valid-email-address-javascript
   isEmail = (email) => {
     let regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    return regex.test
+    return regex.test(email);
   };
 
   function showRequiredFieldError(inputField, errorField) {
